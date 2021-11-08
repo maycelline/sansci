790b7c6533ac100569687de9bc1234cae8f46842
Feliciana Gunadi <isabel.feli27gunadi@gmail.com>
2021-11-08 16:25:34 +0700

    end session (please check again.. someone..)

--- logout.php
+++ logout.php
@@ -1,9 +1,14 @@
 <?php
     session_start();
-    echo "<script>";
-    echo "window.alert('Thanks for using this service!')";
-    echo "</script>";
-    session_unset();
-    session_destroy();
-    header("Location: index.php");
+    if (!$_SESSION) {
+        echo "window.alert('Your session is done, log in first!')";
+        header("Location: index.php");
+    } else {
+        echo "<script>";
+        echo "window.alert('Thanks for using this service!')";
+        echo "</script>";
+        session_unset();
+        session_destroy();
+        header("Location: index.php");
+    }
 ?>
